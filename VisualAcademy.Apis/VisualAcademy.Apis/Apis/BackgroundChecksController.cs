a2c438ae73fd166fe7f78880dfe142d5725de38e
Park YongJune <redplus@redplus.net>
2022-09-19 07:11:18 +0900

    IActionResult and Ok

--- VisualAcademy.Apis/VisualAcademy.Apis/Apis/BackgroundChecksController.cs
+++ VisualAcademy.Apis/VisualAcademy.Apis/Apis/BackgroundChecksController.cs
@@ -5,8 +5,7 @@ namespace VisualAcademy.Apis;
 [Route("api/backgroundchecks")]
 public class BackgroundChecksController : ControllerBase
 {
-    [HttpGet]
-    public object Get()
-        => new { Description = "VisualAcademy Background Check" };
+    [HttpGet(template: "description")]
+    public IActionResult GetDescription()
+        => Ok(new { Description = "VisualAcademy Background Check" });
 }
-
