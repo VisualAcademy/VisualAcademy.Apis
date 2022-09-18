ddf58b73f54cea4ac7bede5c76b7137b9a9e34ab
Park YongJune <redplus@redplus.net>
2022-09-19 06:59:56 +0900

    BackgroundChecksController class and HttpGet attribute

---
+++ VisualAcademy.Apis/VisualAcademy.Apis/Apis/BackgroundChecksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VisualAcademy.Apis;
+
+[Route("api/backgroundchecks")]
+public class BackgroundChecksController : ControllerBase
+{
+    [HttpGet]
+    public object Get()
+        => new { Description = "VisualAcademy Background Check" };
+}
+
